7b71b4cfcebe5a644ac10205c38e0a35932e67bc
Aldar Bazarov <a.bazarov.sas@gmail.com>
2022-11-17 04:51:00 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace FormsCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumber_textBox = new System.Windows.Forms.TextBox();
+            this.firstNumberSystem_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.secondNumberSystem_textBox = new System.Windows.Forms.TextBox();
+            this.secondNumber_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resultNumberSystem_textBox = new System.Windows.Forms.TextBox();
+            this.result_textBox = new System.Windows.Forms.TextBox();
+            this.additional_button = new System.Windows.Forms.Button();
+            this.multiply_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNumber_textBox
+            // 
+            this.firstNumber_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstNumber_textBox.Location = new System.Drawing.Point(37, 55);
+            this.firstNumber_textBox.Name = "firstNumber_textBox";
+            this.firstNumber_textBox.Size = new System.Drawing.Size(189, 27);
+            this.firstNumber_textBox.TabIndex = 0;
+            // 
+            // firstNumberSystem_textBox
+            // 
+            this.firstNumberSystem_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstNumberSystem_textBox.Location = new System.Drawing.Point(318, 55);
+            this.firstNumberSystem_textBox.Name = "firstNumberSystem_textBox";
+            this.firstNumberSystem_textBox.Size = new System.Drawing.Size(189, 27);
+            this.firstNumberSystem_textBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(33, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Первое число:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(314, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "СС превого числа:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(314, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "СС второго числа:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(33, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Второе число:";
+            // 
+            // secondNumberSystem_textBox
+            // 
+            this.secondNumberSystem_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondNumberSystem_textBox.Location = new System.Drawing.Point(318, 123);
+            this.secondNumberSystem_textBox.Name = "secondNumberSystem_textBox";
+            this.secondNumberSystem_textBox.Size = new System.Drawing.Size(189, 27);
+            this.secondNumberSystem_textBox.TabIndex = 5;
+            // 
+            // secondNumber_textBox
+            // 
+            this.secondNumber_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondNumber_textBox.Location = new System.Drawing.Point(37, 123);
+            this.secondNumber_textBox.Name = "secondNumber_textBox";
+            this.secondNumber_textBox.Size = new System.Drawing.Size(189, 27);
+            this.secondNumber_textBox.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(314, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "СС результата:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(33, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Результат:";
+            // 
+            // resultNumberSystem_textBox
+            // 
+            this.resultNumberSystem_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultNumberSystem_textBox.Location = new System.Drawing.Point(318, 194);
+            this.resultNumberSystem_textBox.Name = "resultNumberSystem_textBox";
+            this.resultNumberSystem_textBox.Size = new System.Drawing.Size(189, 27);
+            this.resultNumberSystem_textBox.TabIndex = 9;
+            // 
+            // result_textBox
+            // 
+            this.result_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result_textBox.Location = new System.Drawing.Point(37, 320);
+            this.result_textBox.Name = "result_textBox";
+            this.result_textBox.Size = new System.Drawing.Size(189, 27);
+            this.result_textBox.TabIndex = 8;
+            // 
+            // additional_button
+            // 
+            this.additional_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.additional_button.Location = new System.Drawing.Point(37, 187);
+            this.additional_button.Name = "additional_button";
+            this.additional_button.Size = new System.Drawing.Size(38, 34);
+            this.additional_button.TabIndex = 12;
+            this.additional_button.Text = "+";
+            this.additional_button.UseVisualStyleBackColor = true;
+            this.additional_button.Click += new System.EventHandler(this.additional_button_Click);
+            // 
+            // multiply_button
+            // 
+            this.multiply_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.multiply_button.Location = new System.Drawing.Point(97, 187);
+            this.multiply_button.Name = "multiply_button";
+            this.multiply_button.Size = new System.Drawing.Size(38, 34);
+            this.multiply_button.TabIndex = 13;
+            this.multiply_button.Text = "*";
+            this.multiply_button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.multiply_button.UseVisualStyleBackColor = true;
+            this.multiply_button.Click += new System.EventHandler(this.multiply_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 379);
+            this.Controls.Add(this.multiply_button);
+            this.Controls.Add(this.additional_button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.resultNumberSystem_textBox);
+            this.Controls.Add(this.result_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.secondNumberSystem_textBox);
+            this.Controls.Add(this.secondNumber_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstNumberSystem_textBox);
+            this.Controls.Add(this.firstNumber_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumber_textBox;
+        private System.Windows.Forms.TextBox firstNumberSystem_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox secondNumberSystem_textBox;
+        private System.Windows.Forms.TextBox secondNumber_textBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox resultNumberSystem_textBox;
+        private System.Windows.Forms.TextBox result_textBox;
+        private System.Windows.Forms.Button additional_button;
+        private System.Windows.Forms.Button multiply_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsCalc
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<char, int> symbolToDigit = new Dictionary<char, int>()
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'A', 10 },
+            { 'B', 11 },
+            { 'C', 12 },
+            { 'D', 13 },
+            { 'E', 14 },
+            { 'F', 15 },
+        };
+
+        static Dictionary<int, char> digitToSymbol = new Dictionary<int, char>()
+        {
+            { 0, '0' },
+            { 1, '1' },
+            { 2, '2' },
+            { 3, '3' },
+            { 4, '4' },
+            { 5, '5' },
+            { 6, '6' },
+            { 7, '7' },
+            { 8, '8' },
+            { 9, '9' },
+            { 10, 'A' },
+            { 11, 'B' },
+            { 12, 'C' },
+            { 13, 'D' },
+            { 14, 'E' },
+            { 15, 'F' },
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int ConvertAnyToDecimal(string originNumber, int numberSystem)
+        {
+            double resultDecimalNumber = 0;
+
+            int degree = originNumber.Length - 1;
+
+            for (int i = 0; i < originNumber.Length; i++)
+            {
+                int tmp = symbolToDigit[originNumber[i]];
+                resultDecimalNumber += tmp * Math.Pow(numberSystem, degree);
+                degree--;
+            }
+
+            return (int)resultDecimalNumber;
+        }
+
+        public static string ConvertDecimalToAny(int originNumber, int numberSystem)
+        {
+            string resultNumber = "";
+
+            while (originNumber > 0)
+            {
+                int tmp = originNumber % numberSystem;
+                if (originNumber < numberSystem && originNumber != 0)
+                    resultNumber += digitToSymbol[originNumber];
+                else
+                    resultNumber += digitToSymbol[tmp];
+                originNumber = originNumber / numberSystem;
+            }
+
+            return new string(resultNumber.Reverse().ToArray());
+        }
+
+        public static int Add(int firstNumber, int secondNumber)
+        {
+
+            return firstNumber + secondNumber;
+        }
+
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        private void additional_button_Click(object sender, EventArgs e)
+        {
+            string firstNumber = firstNumber_textBox.Text;
+            string secondNumber = secondNumber_textBox.Text;
+
+            int firstSystem = Convert.ToInt32(firstNumberSystem_textBox.Text);
+            int secondSystem = Convert.ToInt32(secondNumberSystem_textBox.Text);
+
+            int firstNumberInDecimal = ConvertAnyToDecimal(firstNumber, firstSystem);
+            int secondNumberInDecimal = ConvertAnyToDecimal(secondNumber, secondSystem);
+
+            int result = Add(firstNumberInDecimal, secondNumberInDecimal);
+            int resultSystem = Convert.ToInt32(resultNumberSystem_textBox.Text);
+            result_textBox.Text = ConvertDecimalToAny(result, resultSystem);
+        }
+
+        private void multiply_button_Click(object sender, EventArgs e)
+        {
+            string firstNumber = firstNumber_textBox.Text;
+            string secondNumber = secondNumber_textBox.Text;
+
+            int firstSystem = Convert.ToInt32(firstNumberSystem_textBox.Text);
+            int secondSystem = Convert.ToInt32(secondNumberSystem_textBox.Text);
+
+            int firstNumberInDecimal = ConvertAnyToDecimal(firstNumber, firstSystem);
+            int secondNumberInDecimal = ConvertAnyToDecimal(secondNumber, secondSystem);
+
+            int result = Multiply(firstNumberInDecimal, secondNumberInDecimal);
+            int resultSystem = Convert.ToInt32(resultNumberSystem_textBox.Text);
+            result_textBox.Text = ConvertDecimalToAny(result, resultSystem);
+        }
+    }
+}
